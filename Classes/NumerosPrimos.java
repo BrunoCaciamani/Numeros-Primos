2a229bc7cf34514faaaa2e82f11ef833d075c32c
Bruno <106842842+BrunoCaciamani@users.noreply.github.com>
2022-10-12 19:41:30 -0300

    Add files via upload

---
+++ Classes/NumerosPrimos.java
@@ -0,0 +1,35 @@
+package Classes;
+
+public class NumerosPrimos{
+	
+	public boolean verificarPrimo(int num){
+
+		if(num < 0){
+			return false;
+		}
+
+		for(int i = 2; i < num; i++){
+			if((num % i) == 0){
+				return false;
+			}	
+
+		}
+
+		return true;
+
+	}
+	public String verTodos(int num){
+
+		String resultado = "";
+
+		for(int i = 2; i <= num; i++){
+			if(verificarPrimo(i)){
+				resultado += i + " ";
+
+			}
+		}
+
+		return resultado;
+
+	}
+}
